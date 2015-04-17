6db45e4e5f8b3b58970939af17aa281a2c2636d7
Ibrahim Eid <humazed@gmail.com>
2015-04-17 20:17:04 +0200

    make LecturesFragment and SectionsFragment display weeks.

--- app/src/main/java/com/example/huma/almalzma/subject/LecturesFragment.java
+++ app/src/main/java/com/example/huma/almalzma/subject/LecturesFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.huma.almalzma.R;
 
@@ -24,6 +25,11 @@ public class LecturesFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String[] weeks = getResources().getStringArray(R.array.weeks);
+
+        setListAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, weeks));
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lectures, container, false);
     }
